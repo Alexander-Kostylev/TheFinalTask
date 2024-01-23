1b1b5e629e7171ff588692333234038810474017
Alexander Kostylev <successful08@mail.ru>
2024-01-23 23:28:43 +0500

    start final task

--- TheFinalTask.cs
+++ TheFinalTask.cs
@@ -2,3 +2,20 @@
 // Создание нового массива строк, из имеющегося,
 // длинна строки которого меньше или равна 3 символам.
 
+string [] ReadData(string mes)
+{
+    Console.WriteLine(mes);
+
+    int countArr = int.Parse(Console.ReadLine() ?? "0");
+    string[] arr = new string [countArr];
+    
+    for (int i = 0; i < countArr; i++)
+    {
+        arr[i] = Console.ReadLine();
+    }
+
+    return arr;
+}
+
+
+string [] startArr = ReadData("Укажите количество строк в массиве");
