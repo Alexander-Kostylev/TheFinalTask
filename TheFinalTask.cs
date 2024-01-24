f184b80c42195ddd20ff4a4245880867516f55d7
Alexander Kostylev <successful08@mail.ru>
2024-01-24 23:13:00 +0500

    program code done

--- TheFinalTask.cs
+++ TheFinalTask.cs
@@ -17,5 +17,54 @@ string [] ReadData(string mes)
     return arr;
 }
 
+string [] Filter(string [] tempArr)
+{
+    int count = 0;
+    
+    for(int i = 0; i < tempArr.Length; i++)
+    {
+        if(tempArr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string [] resultArr = new string [count];
+    int tempIndex = 0;
+
+    for(int i = 0; i < tempArr.Length; i++)
+    {
+        if(tempArr[i].Length <= 3)
+        {
+            resultArr[tempIndex] = tempArr[i];
+            tempIndex++;
+        }
+    }
+    
+    return resultArr;
+}
+
+void PrintData(string [] array)
+{   
+    if(array.Length == 0)
+    {
+        Console.Write("[]");
+    }
+    else if(array.Length == 1)
+    {
+        Console.Write("[" + array[0] + "]");
+    }
+    else{
+        Console.Write("[ ");
+        for( int i = 0; i < array.Length - 1; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        Console.Write(array[array.Length - 1] + "]");
+    }
+}
 
 string [] startArr = ReadData("Укажите количество строк в массиве");
+string [] endArr = Filter(startArr);
+PrintData(endArr);
+
